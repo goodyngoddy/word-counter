f8ff01cc2f3a6c88dd6e6f36bd9402d0c937f8ee
goodnewsngodspower@gmail.com <goodnewsngodspower@gmail.com>
2023-10-19 20:59:45 +0100

    updated counter app

--- main.js
+++ main.js
@@ -1,29 +1,36 @@
-let input = document.querySelector('input')
+let textarea = document.querySelector('textarea')
 let button = document.querySelector('button')
 let wordCount = document.querySelector('.word-count')
+let charCount = document.querySelector('.char-count')
 
 let alphaList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 button.addEventListener('click', () => {
-  console.log(input.value)
   countWords()
 })
+
+let wordCountNum
+
 function countWords() {
-  let inputValue = input.value
-  let count = 0
+  let inputValue = textarea.value
+  let wordNum = 0
+  let charNum = 0
+  
   for (var i = 0; i < inputValue.length; i++) {
+    charNum = i
     if (!alphaList.includes(inputValue[i])) {
       if (alphaList.includes(inputValue[i - 1])) {
-        count++
+        wordNum++
       }
-      wordCount.textContent = count
-      console.log(inputValue[i])
     }
   }
+  if (alphaList.includes(inputValue[inputValue.length - 1])) {
+    wordCountNum = wordNum + 1
+  } else {
+    wordCountNum = wordNum 
+  }
+  wordCount.textContent = wordCountNum
+  charCount.textContent = charNum + 1
 }
 
 
-
-// '-', '"', '.', ',', '`', ';', ':'
-
-// London. Michaelmas term lately over, and the Lord Chancellor sitting in Lincoln's Inn Hall. Implacable November weather. As much mud in the streets as if the waters had but newly retired from the face of the earth, and it would not be wonderful to meet a Megalosaurus, forty feet long or so, waddling like an elephantine lizard up Holborn Hill.
